59bd4c8b5aacec981277c173e84242564d862fef
prakashiosys <55125959+prakashiosys@users.noreply.github.com>
2019-10-18 21:12:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+	      $('.slider').slick({
+	      	slidesToShow: 3,
+	      	slidesToScroll: 3,
+	        accessibilty: true,
+	        dots: true,
+	        arrows: false,
+	        centerMode: true,
+		    infinite: true,
+		    cssEase: 'linear'
+	    });
+	});
